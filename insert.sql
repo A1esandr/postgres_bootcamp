c2d104fb61005a1ab16cdebf1df08bc8bab905cb
Alexander Kurakin <A1esandr@users.noreply.github.com>
2022-01-05 19:01:50 +0500

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,12 @@
+CREATE TABLE customers (
+	customer_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(150),
+	age INT
+);
+
+SELECT * FROM customers;
+
+INSERT INTO customers (first_name, last_name, email, age)
+VALUES ('Alex','K','a@k.com',50);
