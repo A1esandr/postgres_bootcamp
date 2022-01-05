accae1691ee1631b6c63a4ab16f839cf13e77112
Alexander Kurakin <A1esandr@users.noreply.github.com>
2022-01-06 01:44:58 +0500

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -10,3 +10,10 @@ SELECT * FROM customers;
 
 INSERT INTO customers (first_name, last_name, email, age)
 VALUES ('Alex','K','a@k.com',50);
+
+INSERT INTO customers (first_name, last_name, email, age)
+VALUES 
+('Bob','Joe','b@k.com',30),
+('John','Smith','j@k.com',40),
+('Kate','Dc','d@k.com',34)
+RETURNING customer_id;
